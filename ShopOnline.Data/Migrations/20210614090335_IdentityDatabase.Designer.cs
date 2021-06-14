f10be0896f4f366f30a357a2a9ec7250b97e70a8
DESKTOP-O4NQ6OQ\TUAN VI <trantuanvi1999@gmail.com>
2021-06-14 17:02:22 +0700

    seeding data for user and role

--- ShopOnline.Data/Migrations/20210614090335_IdentityDatabase.Designer.cs
+++ ShopOnline.Data/Migrations/20210614090335_IdentityDatabase.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopOnline.Data.EF;
 
 namespace ShopOnline.Data.Migrations
 {
     [DbContext(typeof(EshopDbContext))]
-    partial class EshopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210614090335_IdentityDatabase")]
+    partial class IdentityDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
