4bd6fedffa9c7e9f49f96d4e0b759b503c83166a
DESKTOP-O4NQ6OQ\TUAN VI <trantuanvi1999@gmail.com>
2021-06-17 17:34:40 +0700

    Add swagger for backEndApi

---
+++ ShopOnline.BackEndApi/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Application.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnline.BackEndApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+        public ProductController (ProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
